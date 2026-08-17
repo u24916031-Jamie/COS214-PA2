2fbb5d324bc74e03144d682f43136d17fa38c32d
wbotha1019-crypto <wbotha1019@gmail.com>
2026-08-17 12:16:56 +0200

    Refactor Settlement class and include Traveller forward declaration

--- src/Settlement.h
+++ src/Settlement.h
@@ -2,26 +2,31 @@
 #define SETTLEMENT_H
 
 #include "Place.h"
-#include "Traveller.h"
+#include <string>
 
-class Settlement : Place {
+class Traveller;
 
+class Settlement : public Place 
+{
+protected:
+	std::string enterMessage;
+	std::string shopMessage;
+	std::string sleepMessage;
+	std::string workMessage;
+	std::string eatMessage;
 
+	Settlement(const std::string& name, const std::string& description,const std::string& enterMessage, const std::string& shopMessage,const std::string& sleepMessage, const std::string& workMessage,const std::string& eatMessage);
 
 public:
-	Settlement() = delete;
-	Settlement(std::string name, std::string description) : Place(name, description, true) {};
-
+	virtual ~Settlement();
+	virtual bool isSettlement() const;
+	virtual void print(int depth = 0) const;
 
 	virtual void enter(Traveller& traveller) = 0;
-	virtual void shop(Traveller& traveller) = 0;
-	virtual void sleep(Traveller& traveller) = 0;
-	virtual void work(Traveller& traveller) = 0;
-	virtual void eat(Traveller& traveller) = 0;
-
-	virtual void addSettlement(Settlement* param) = 0;
-
-	virtual ~Settlement() {};
+	virtual void shop(Traveller& traveller);
+	virtual void sleep(Traveller& traveller);
+	virtual void work(Traveller& traveller);
+	virtual void eat(Traveller& traveller);
 };
 
 #endif
