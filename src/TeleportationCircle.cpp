89bbaf96f17e1cdea56429f3d80dc5d13937749b
wbotha1019-crypto <wbotha1019@gmail.com>
2026-08-17 11:00:05 +0200

    Implement leave method in TeleportationCircle
    Implement leave method for TeleportationCircle class to handle insufficient funds and transition to MerchantCaravan.

--- src/TeleportationCircle.cpp
+++ src/TeleportationCircle.cpp
@@ -1,6 +1,19 @@
 #include "TeleportationCircle.h"
+#include "MerchantCaravan.h"
+#include "Traveller.h"
+#include <iostream>
 
-boolean TeleportationCircle::leave(Traveller& traveller) {
-	// TODO - implement TeleportationCircle::leave
-	throw "Not yet implemented";
+TeleportationCircle::TeleportationCircle(): TransportationMethod(5, 100,"The circle flares and you step out at your destination in an instant.","The circle stays dark. You cannot pay the mages.") {}
+
+TeleportationCircle::~TeleportationCircle() {}
+
+bool TeleportationCircle::leave(Traveller& traveller) 
+{
+	if (traveller.getMoney() < 100) 
+	{
+		std::cout << "You can no longer afford the Teleportation Circle. You join a Merchant Caravan." << std::endl;
+		traveller.setTransportationMethod(new MerchantCaravan());
+		return traveller.leave();
+	}
+	return TransportationMethod::leave(traveller);
 }
